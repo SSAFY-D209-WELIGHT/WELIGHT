b0bcc25ebd3f835aaeeaa9df64a6972c89a64b2c
김성수 <skele1207@gmail.com>
2024-10-29 16:27:12 +0900

    fix : internal 제거 및 extension 적용

--- Android/build-logic/convention/src/main/java/com/rohkee/build_logic/convention/AndroidApplicationConventionPlugin.kt
+++ Android/build-logic/convention/src/main/java/com/rohkee/build_logic/convention/AndroidApplicationConventionPlugin.kt
@@ -1,17 +1,22 @@
+import com.android.build.api.dsl.ApplicationExtension
 import com.rohkee.build_logic.convention.extension.configureKotlinAndroid
 import com.rohkee.build_logic.convention.extension.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
-internal class AndroidApplicationConventionPlugin : Plugin<Project> {
+class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             apply(plugin = "com.android.application")
             apply(plugin = "org.jetbrains.kotlin.android")
 
-            configureKotlinAndroid()
+            configure<ApplicationExtension>{
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 34
+            }
 
             dependencies {
                 add("implementation", libs.findBundle("android.core").get())
--- Android/build-logic/convention/src/main/java/com/rohkee/build_logic/convention/HiltConventionPlugin.kt
+++ Android/build-logic/convention/src/main/java/com/rohkee/build_logic/convention/HiltConventionPlugin.kt
@@ -3,7 +3,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 
-internal class HiltConventionPlugin : Plugin<Project> {
+class HiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager){
