a9bbb9df545a33e590131bada1455a45748de2d8
김성수 <skele1207@gmail.com>
2024-10-29 17:57:27 +0900

    feat : 보관함 화면 작업중

--- Android/core/ui/src/main/java/com/rohkee/core/ui/storage/StorageContent.kt
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/storage/StorageContent.kt
@@ -1,2 +1,90 @@
 package com.rohkee.core.ui.storage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/**
+ * 보관함 화면
+ */
+@Composable
+fun StorageContent(
+    modifier: Modifier = Modifier,
+    state: StorageState,
+    onIntent: (StorageIntent) -> Unit = {},
+) {
+    Column(modifier = modifier) {
+        when (state) {
+            is StorageState.Loading -> {
+            }
+
+            is StorageState.Loaded -> {
+            }
+
+            is StorageState.NoData -> {
+            }
+
+            is StorageState.Error -> {
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateDisplayButton(modifier: Modifier = Modifier) {
+    Box(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(16.dp),
+                ),
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Surface(
+                modifier = Modifier.size(36.dp),
+                shape = CircleShape,
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                )
+            }
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "추가",
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CreateDisplayButtonPreview() {
+    CreateDisplayButton()
+}
---
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/storage/StorageState.kt
@@ -0,0 +1,31 @@
+package com.rohkee.core.ui.storage
+
+/**
+ * 보관함 화면 상태
+ */
+sealed interface StorageState {
+    /**
+     * 로딩중
+     */
+    data object Loading : StorageState
+
+    /**
+     * 로딩 완료
+     * 저장된 응원도구가 존재
+     */
+    data class Loaded(
+        val imgUrls: List<String>,
+    ) : StorageState
+
+    /**
+     * 저장된 응원도구 데이터 없음
+     */
+    data object NoData : StorageState
+
+    /**
+     * 에러
+     */
+    data class Error(
+        val message: String,
+    ) : StorageState
+}
