77b864785a7fb0b87d7105cb469d07cfb7b407ed
김성수 <skele1207@gmail.com>
2024-10-29 16:27:38 +0900

    feat : Compose 적용 Application 및 Library 플러그인 추가

---
+++ Android/build-logic/convention/src/main/java/com/rohkee/build_logic/convention/AndroidApplicationComposeConventionPlugin.kt
@@ -0,0 +1,31 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.rohkee.build_logic.convention.extension.configureAndroidCompose
+import com.rohkee.build_logic.convention.extension.configureKotlinAndroid
+import com.rohkee.build_logic.convention.extension.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.application")
+            apply(plugin = "org.jetbrains.kotlin.android")
+
+            configure<ApplicationExtension>{
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 34
+                configureAndroidCompose(this)
+            }
+
+            dependencies {
+                add("implementation", libs.findBundle("android.core").get())
+
+                // timber logger
+                add("implementation", libs.findLibrary("timber").get())
+            }
+        }
+    }
+}
