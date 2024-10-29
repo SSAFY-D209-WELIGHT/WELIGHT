59d8c15d545b27930dfd73c5bd32a5f3d22e7437
SeungSoo <skele1207@gmail.com>
2024-10-30 00:33:22 +0900

    feat : 페이저 추가

---
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/storage/component/HorizontalView.kt
@@ -0,0 +1,33 @@
+package com.rohkee.core.ui.storage.component
+
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HorizontalView(modifier: Modifier = Modifier) {
+    val pagerState =
+        rememberPagerState(
+            initialPage = 0,
+            pageCount = { 10 },
+        )
+
+    HorizontalPager(
+        modifier = modifier,
+        state = pagerState,
+        pageSize = PageSize.Fixed(100.dp)
+    ) { page ->
+        Text("$page")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HorizontalViewPreview() {
+    HorizontalView()
+}
