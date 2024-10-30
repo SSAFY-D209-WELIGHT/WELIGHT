8a970d9d81485cfed277ca9288ac15cefb478633
SeungSoo <skele1207@gmail.com>
2024-10-31 00:33:20 +0900

    chore : gradle 수정

--- Android/core/network/build.gradle.kts
+++ Android/core/network/build.gradle.kts
@@ -1,43 +1,28 @@
+import java.util.Properties
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.module.android.library)
+    alias(libs.plugins.module.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
+val properties = Properties()
+properties.load(rootProject.file("local.properties").inputStream())
+
+
 android {
-    namespace = "com.rokhee.core.network"
-    compileSdk = 34
-
+    namespace = "com.rohkee.core.network"
     defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+        buildConfigField("String", "BASE_URL", properties["BASE_URL"] as String)
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
+    buildFeatures {
+        buildConfig = true
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.bundles.network)
+    implementation(libs.bundles.serialization)
+    implementation(project(":core:datastore"))
 }
