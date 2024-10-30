9d15aa1876c0c78a25e746b95a10b14c436d93ef
김성수 <skele1207@gmail.com>
2024-10-30 10:17:04 +0900

    feat : 글로벌 색상 추가

---
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.rohkee.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val Background = Color(0xFF1E1E1E)
+    val OnBackground = Color(0xFFF5F5F5)
+    val BackgroundTransparent = Color(0xB31E1E1E)
+    val OnBackgroundTransparent = Color(0x80F5F5F5)
+    val Surface = Color(0xFF252525)
+    val OnSurface = Color(0xFFF4F4F4)
+    val SurfaceTransparent = Color(0x80272727)
+    val OverSurface = Color(0x802C2C2C)
+    val Contrast = Color(0xFFF0F0F0)
+    val OnContrast = Color(0xFF272727)
+    val Convex = Color(0xFFD9D9D9)
+    val OnConvex = Color(0xFF575757)
+    val Active = Color(0xFF555555)
+    val Inactive = Color(0xFFA87FEE)
+    val Warning = Color(0xFFE23A3A)
+}
