b04beaf232cc1ff828ee5191f00b2447dd4eebfd
김성수 <skele1207@gmail.com>
2024-10-31 17:25:01 +0900

    feat : 피쳐 모듈 구현

---
+++ Android/feat/storage/src/main/java/com/rohkee/feat/storage/util/FlowExtension.kt
@@ -0,0 +1,25 @@
+package com.rohkee.feat.storage.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+
+@Composable
+fun <T> Flow<T>.collectWithLifecycle(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    action: FlowCollector<T>,
+) {
+    LaunchedEffect(Unit) {
+        this@collectWithLifecycle
+            .flowWithLifecycle(
+                lifecycleOwner.lifecycle,
+                minActiveState,
+            ).collect(action)
+    }
+}
