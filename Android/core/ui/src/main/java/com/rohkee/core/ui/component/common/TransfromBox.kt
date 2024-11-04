da5c16e90cd19cecf7cb6cfa0eae44dba5ec4357
김성수 <skele1207@gmail.com>
2024-11-04 17:24:54 +0900

    feat : 회전, 스케일, 이동 가능한 컴포넌트 추가

---
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/component/common/TransfromBox.kt
@@ -0,0 +1,106 @@
+package com.rohkee.core.ui.component.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * 스케일, 회전, 이동이 가능한 Box
+ *
+ * @param scale : 스케일 ( 1.0f = 100% 크기 )
+ * @param rotation : 회전 각도 ( -360 ~ 360 )
+ * @param offset : 화면 크기 비례 이동량 ( 0.0(제자리) 0.5f(화면 중앙) ~ 1.0f(화면끝) )
+ */
+@Composable
+fun TransformableBox(
+    modifier: Modifier = Modifier,
+    scale: Float,
+    rotation: Float,
+    offset: Offset,
+    onTransfrm: ((scale: Float, rotation: Float, offset: Offset) -> Unit)? = null,
+    content: @Composable () -> Unit,
+) {
+    BoxWithConstraints {
+        val density = LocalDensity.current
+        val width = with(density) { maxWidth.toPx() }
+        val height = with(density) { maxHeight.toPx() }
+
+        val state =
+            rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+                onTransfrm?.invoke(
+                    scale * zoomChange,
+                    rotation + rotationChange,
+                    offset +
+                        Offset(
+                            x = offsetChange.x / width,
+                            y = offsetChange.y / height,
+                        ),
+                )
+            }
+
+        Box(
+            modifier
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    rotationZ = rotation,
+                    translationX = (offset.x * width),
+                    translationY = (offset.y * height),
+                ).then(
+                    if (onTransfrm != null) {
+                        Modifier.transformable(state = state)
+                    } else {
+                        Modifier
+                    },
+                ),
+        ) {
+            content()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TransformableBoxPreview() {
+    var scale by remember { mutableFloatStateOf(1f) }
+    var rotation by remember { mutableFloatStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+
+    Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        TransformableBox(
+            scale = scale,
+            rotation = rotation,
+            offset = offset,
+            onTransfrm = { s, r, o ->
+                scale = s
+                rotation = r
+                offset = o
+            },
+        ) {
+            Text(
+                modifier = Modifier.background(color = Color.Cyan),
+                text = "$scale $rotation $offset",
+                fontSize = 40.sp,
+            )
+        }
+    }
+}
