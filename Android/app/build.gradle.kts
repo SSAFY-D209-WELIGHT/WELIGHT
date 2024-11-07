a4b78a0ca63cb5309bb936d9ce0ebaef1ed55aa5
김성수 <skele1207@gmail.com>
2024-11-07 18:05:17 +0900

    feat : Hilt module 적용

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -31,4 +31,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.core)
     //compose-navigation
     implementation(libs.androidx.navigation.compose)
+
+    implementation(project(":feat:storage"))
+    implementation(project(":feat:display"))
 }
