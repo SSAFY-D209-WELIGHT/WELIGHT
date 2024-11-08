1a37f782a7be43afc2083446be4a5cc97d5243f8
이재영 <boutljy@naver.com>
2024-11-08 14:57:01 +0900

    fix: @SpringBootTest 주석 처리

--- Backend/src/test/java/com/d209/welight/WelightApplicationTests.java
+++ Backend/src/test/java/com/d209/welight/WelightApplicationTests.java
@@ -1,13 +1,13 @@
-package com.d209.welight;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WelightApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.d209.welight;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WelightApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
