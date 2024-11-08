cfe0605d9902c1f5831b554308b572361fd0b2df
김성수 <skele1207@gmail.com>
2024-11-08 10:42:56 +0900

    refactor : 시스템 바 색상 적용

--- Android/core/ui/src/main/java/com/rohkee/core/ui/theme/Theme.kt
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/theme/Theme.kt
@@ -1,12 +1,14 @@
 package com.rohkee.core.ui.theme
 
-import android.app.Activity
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.platform.LocalContext
 
 private val ColorScheme =
     darkColorScheme(
@@ -17,13 +19,23 @@ private val ColorScheme =
 
 @Composable
 fun WeLightTheme(content: @Composable () -> Unit) {
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = AppColor.BackgroundTransparent.toArgb()
-            window.navigationBarColor = AppColor.BackgroundTransparent.toArgb()
-        }
+//    val view = LocalView.current
+//    if (!view.isInEditMode) {
+//        SideEffect {
+//            val window = (view.context as Activity).window
+//            window.statusBarColor = AppColor.BackgroundTransparent.toArgb()
+//            window.navigationBarColor = AppColor.BackgroundTransparent.toArgb()
+//        }
+//    }
+    val context = LocalContext.current as ComponentActivity
+
+    DisposableEffect(Unit) {
+        context.enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(AppColor.BackgroundTransparent.toArgb()),
+            navigationBarStyle = SystemBarStyle.dark(AppColor.BackgroundTransparent.toArgb()),
+        )
+
+        onDispose {  }
     }
 
     MaterialTheme(
