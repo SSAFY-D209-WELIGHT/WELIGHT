bcffdf92094ff4550606fa20385a7c5f8e9149df
김성수 <skele1207@gmail.com>
2024-11-08 14:34:49 +0900

    chore : 의존성 추가

--- Android/core/network/build.gradle.kts
+++ Android/core/network/build.gradle.kts
@@ -14,6 +14,9 @@ android {
     namespace = "com.rohkee.core.network"
     defaultConfig {
         buildConfigField("String", "BASE_URL", properties["BASE_URL"] as String)
+        buildConfigField("String", "BUCKET_NAME", properties["BUCKET_NAME"] as String)
+        buildConfigField("String", "AWS_ACCESS_KEY", properties["AWS_ACCESS_KEY"] as String)
+        buildConfigField("String", "AWS_SECRET_KEY", properties["AWS_SECRET_KEY"] as String)
     }
 
     buildFeatures {
@@ -25,4 +28,8 @@ dependencies {
     implementation(libs.bundles.network)
     implementation(libs.bundles.serialization)
     implementation(project(":core:datastore"))
+
+    // s3
+    implementation(libs.aws.android.sdk.s3)
+    implementation(libs.aws.android.sdk.core)
 }
