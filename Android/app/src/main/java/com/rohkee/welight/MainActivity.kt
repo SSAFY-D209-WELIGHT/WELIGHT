044c110b12f1f9153f3433b05140c6881b952bc7
김성수 <skele1207@gmail.com>
2024-11-08 13:06:54 +0900

    refactor : enableEdgeToEdge를 각 모듈로 이동

--- Android/app/src/main/java/com/rohkee/welight/MainActivity.kt
+++ Android/app/src/main/java/com/rohkee/welight/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.rohkee.core.ui.theme.WeLightTheme
 import com.rohkee.welight.navigation.MainNavigation
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,11 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeLightTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainNavigation(
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                MainNavigation()
             }
         }
     }
--- Android/core/ui/src/main/java/com/rohkee/core/ui/theme/Theme.kt
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/theme/Theme.kt
@@ -1,14 +1,8 @@
 package com.rohkee.core.ui.theme
 
-import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 
 private val ColorScheme =
     darkColorScheme(
@@ -19,25 +13,6 @@ private val ColorScheme =
 
 @Composable
 fun WeLightTheme(content: @Composable () -> Unit) {
-//    val view = LocalView.current
-//    if (!view.isInEditMode) {
-//        SideEffect {
-//            val window = (view.context as Activity).window
-//            window.statusBarColor = AppColor.BackgroundTransparent.toArgb()
-//            window.navigationBarColor = AppColor.BackgroundTransparent.toArgb()
-//        }
-//    }
-    val context = LocalContext.current as ComponentActivity
-
-    DisposableEffect(Unit) {
-        context.enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(AppColor.BackgroundTransparent.toArgb()),
-            navigationBarStyle = SystemBarStyle.dark(AppColor.BackgroundTransparent.toArgb()),
-        )
-
-        onDispose {  }
-    }
-
     MaterialTheme(
         colorScheme = ColorScheme,
         typography = Typography,
