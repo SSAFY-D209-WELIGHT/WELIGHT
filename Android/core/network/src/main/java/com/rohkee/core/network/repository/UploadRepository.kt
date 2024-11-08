adff76c0109da3b84e215857fc0f2c6bfc5616d2
김성수 <skele1207@gmail.com>
2024-11-08 14:35:05 +0900

    feature : repository 추가

---
+++ Android/core/network/src/main/java/com/rohkee/core/network/repository/UploadRepository.kt
@@ -0,0 +1,13 @@
+package com.rohkee.core.network.repository
+
+import com.rohkee.core.network.ApiResponse
+import com.rohkee.core.network.model.Upload
+import kotlinx.coroutines.flow.Flow
+import java.io.File
+
+interface UploadRepository {
+    suspend fun upload(
+        key: String,
+        file: File,
+    ): Flow<ApiResponse<Upload>>
+}
