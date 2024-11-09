293698bdbcce345e8acae4d05c3301dd7d8ed9f9
김성수 <skele1207@gmail.com>
2024-11-09 17:09:09 +0900

    chore : 페이징 의존성 추가

--- Android/feat/storage/build.gradle.kts
+++ Android/feat/storage/build.gradle.kts
@@ -8,6 +8,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose.android)
     implementation(project(":core:network"))
     implementation(project(":core:ui"))
 }
