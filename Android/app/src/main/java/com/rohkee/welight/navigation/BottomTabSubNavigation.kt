1da96bda71e0dd2d4e3cf91565f1d00e8a855f39
김성수 <skele1207@gmail.com>
2024-11-11 22:26:59 +0900

    feature : 네비게이션 수정

--- Android/app/src/main/java/com/rohkee/welight/navigation/BottomTabSubNavigation.kt
+++ Android/app/src/main/java/com/rohkee/welight/navigation/BottomTabSubNavigation.kt
@@ -1,12 +1,20 @@
 package com.rohkee.welight.navigation
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.rohkee.core.ui.component.common.BottomNavigationBar
+import com.rohkee.core.ui.component.common.BottomNavigationItemState
+import com.rohkee.feat.storage.Storage
 import com.rohkee.feat.storage.StorageScreen
+import com.rohkee.welight.R
 
 @Composable
 fun BottomTabSubNavigation(
@@ -15,18 +23,50 @@ fun BottomTabSubNavigation(
     onNavigateToDisplayDetail: (id: Long) -> Unit,
     onNavigateToCreateNewDisplay: () -> Unit,
 ) {
-    NavHost(navController = navController, startDestination = Storage) {
-        composable<Storage> {
-            StorageScreen(
-                onNavigateToDisplayDetail = onNavigateToDisplayDetail,
-                onNavigateToCreateNewDisplay = onNavigateToCreateNewDisplay,
-            )
-        }
-        composable<Group> {
-        }
-        composable<Board> {
-        }
-        composable<MyPage> {
+    val bottomRoute by navController.currentBackStackEntryAsState()
+
+    Scaffold(modifier = modifier, bottomBar = {
+        BottomNavigationBar(
+            startDestination = bottomRoute?.destination?.route,
+            items =
+                listOf(
+                    BottomNavigationItemState(
+                        route = Storage,
+                        label = "보관함",
+                        icon = R.drawable.ic_storage_unselected,
+                        selectedIcon = R.drawable.ic_storage_selected,
+                    ),
+                    BottomNavigationItemState(
+                        route = "group",
+                        label = "단체 응원",
+                        icon = R.drawable.ic_group_unselected,
+                        selectedIcon = R.drawable.ic_group_selected,
+                    ),
+                    BottomNavigationItemState(
+                        route = "board",
+                        label = "게시판",
+                        icon = R.drawable.ic_board_unselected,
+                        selectedIcon = R.drawable.ic_board_selected,
+                    ),
+                ),
+            onSelected = { route ->
+                navController.navigate(route) {
+                    popUpTo(navController.graph.findStartDestination().id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            },
+        )
+    }) { innerPadding ->
+        NavHost(navController = navController, startDestination = Storage) {
+            composable<Storage> {
+                StorageScreen(
+                    onNavigateToDisplayDetail = onNavigateToDisplayDetail,
+                    onNavigateToCreateNewDisplay = onNavigateToCreateNewDisplay,
+                )
+            }
         }
     }
 }
---
+++ Android/feat/storage/src/main/java/com/rohkee/feat/storage/StorageRoute.kt
@@ -0,0 +1,6 @@
+package com.rohkee.feat.storage
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Storage
