05e7e5a1d310fd743a30b42a718ab90f9cb5affd
김성수 <skele1207@gmail.com>
2024-11-12 15:01:55 +0900

    feature : 클라이언트 화면 구현

---
+++ Android/feature/group/src/main/java/com/rohkee/feature/group/client/ClientScreen.kt
@@ -0,0 +1,29 @@
+package com.rohkee.feature.group.client
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.rohkee.core.ui.util.collectWithLifecycle
+
+@Composable
+fun ClientScreen(
+    modifier: Modifier = Modifier,
+    clientViewModel: ClientViewModel = hiltViewModel(),
+    onPopBackStack: () -> Unit = {},
+) {
+    val clientUiState by clientViewModel.clientState.collectAsStateWithLifecycle()
+
+    clientViewModel.clientEvent.collectWithLifecycle { event ->
+        when (event) {
+            ClientEvent.ExitPage -> onPopBackStack()
+        }
+    }
+
+    ClientContent(
+        modifier = modifier,
+        state = clientUiState,
+        onIntent = clientViewModel::onIntent,
+    )
+}
