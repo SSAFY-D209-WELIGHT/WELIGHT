b370cda48d07605ea044ddc4e808186176fc6806
노나현 <selene0106@naver.com>
2024-11-13 17:43:22 +0900

    fix: display like list응답에서 favorite 컬럼 제외

---
+++ Backend/src/main/java/com/d209/welight/domain/display/dto/response/DisplayLikedListResponse.java
@@ -0,0 +1,26 @@
+package com.d209.welight.domain.display.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DisplayLikedResponse {
+    private int currentPage;
+    private List<DisplayInfo> displays;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DisplayInfo {
+        private Long displayUid;
+        private String displayThumbnail;
+    }
+}
