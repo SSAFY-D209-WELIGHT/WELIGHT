f800e536212cdf2442b4248714aac299c3b94c17
노진서 <nao16@naver.com>
2024-11-14 21:34:53 +0900

    feat: 게시판 기능(api 시도)

--- Android/feature/board/src/main/java/com/rohkee/feature/board/BoardContent.kt
+++ Android/feature/board/src/main/java/com/rohkee/feature/board/BoardContent.kt
@@ -1,5 +1,6 @@
 package com.rohkee.feature.board
-
+import com.rohkee.core.network.repository.DisplayRepository
+import com.rohkee.core.network.repository.SortType
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.material.icons.Icons
@@ -12,10 +13,19 @@ import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import com.rohkee.feature.board.model.Board
+import androidx.paging.PagingData
+import com.rohkee.core.network.model.DisplayResponse
+import kotlinx.coroutines.flow.Flow
 
 data class BoardContent(
-    val boards: List<Board> = emptyList(),
+    val boards: Flow<PagingData<DisplayResponse.Short>> = Flow { PagingData.empty() },
     val searchQuery: String = "",
-    val isSearchVisible: Boolean = false
+    val isSearchVisible: Boolean = false,
+    val sortType: SortType = SortType.LATEST
 )
-
+//data class BoardContent(
+//    val boards: List<Board> = emptyList(),
+//    val searchQuery: String = "",
+//    val isSearchVisible: Boolean = false
+//)
+//
