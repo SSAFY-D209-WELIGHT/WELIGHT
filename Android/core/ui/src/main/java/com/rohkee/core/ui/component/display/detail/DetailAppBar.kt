004669d127734d53e8678fbd220dec86a01e7663
김성수 <skele1207@gmail.com>
2024-11-18 16:43:58 +0900

    fix : wrong resource

--- Android/core/ui/src/main/java/com/rohkee/core/ui/component/display/detail/DetailAppBar.kt
+++ Android/core/ui/src/main/java/com/rohkee/core/ui/component/display/detail/DetailAppBar.kt
@@ -82,7 +82,7 @@ fun DefaultDetailAppBar(
     onDeleteClick: () -> Unit = {},
 ) {
     val favorite =
-        remember(state.isFavorite) { if (state.isFavorite) R.drawable.favorite_star else R.drawable.favorite_star_filled }
+        remember(state.isFavorite) { if (state.isFavorite) R.drawable.favorite_star_filled else R.drawable.favorite_star }
 
     val (openDropdown, setOpenDropdown) = remember { mutableStateOf(false) }
 
